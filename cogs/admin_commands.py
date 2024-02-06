2e58de5fa7e73a343b9004d4637bf7a4f164aabc
Preetinder Singh <preetindersingh13per@gmail.com>
2024-02-06 12:58:05 +0530

    Added command to assign partcipant role

--- cogs/admin_commands.py
+++ cogs/admin_commands.py
@@ -1,10 +1,24 @@
 from discord import Interaction, app_commands
 from discord.ext import commands
 from bot import MlscBot
+from pandas import read_csv
+from csv import DictReader
+from discord.utils import get
 
 class AdminCommands(commands.Cog):
     def __init__(self, bot: MlscBot):
         self.bot = bot
+
+    @app_commands.command()
+    async def assign_participants_role(self, inter: Interaction):
+        guild = inter.guild
+        with open('Makeathon.csv', 'r') as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader:
+                for member in guild.members:
+                    if str(member.id) == row["Discord Ids"]:
+                        team_role = get(guild.roles, name="Participant")
+                        await member.add_roles(team_role)
 
     @app_commands.command()
     async def serverid(self, inter: Interaction):
--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -1,10 +1,7 @@
 from discord import Interaction, app_commands, Member
 from discord.ext import commands
-from discord.utils import get
 from bot import MlscBot
 import json
-from pandas import read_csv
-from csv import DictReader
 
 class Welcomer(commands.Cog):
     def __init__(self, bot: MlscBot):
@@ -14,14 +11,6 @@ class Welcomer(commands.Cog):
     async def on_member_join(self, member: Member):
         with open("./data.json", "r") as data:
             records = json.load(data)
-
-        guild = member.guild
-        with open('Makeathon.csv', 'r') as csvfile:
-            reader = DictReader(csvfile)
-            for row in reader:
-                if str(member.id) == row["Discord Ids"]:
-                    team_role = get(guild.roles, name="Participant")
-                    await member.add_roles(team_role)
 
         try:
             channel_id = records[str(member.guild.id)]
@@ -34,11 +23,6 @@ class Welcomer(commands.Cog):
         
         await channel.send(f"Welcome {member.mention}!")
 
-    @app_commands.command()
-    async def read_cvs(self, inter: Interaction):
-        
-
-        await inter.response.send_message("done", ephemeral=True)
     
     @app_commands.command()
     async def welcome(self, inter: Interaction):
