8efeaf7d6712eec239ded2600b3ec464e703e61b
Preetinder Singh <preetindersingh13per@gmail.com>
2024-02-11 18:03:45 +0530

    Final commit

--- cogs/team_manager.py
+++ cogs/team_manager.py
@@ -46,8 +46,6 @@ class TeamManager(commands.Cog):
                     doc_ref = db.collection("Discord_Users").document("Teams")
                     database_team_set = set()
 
-                    
-
                     doc = doc_ref.get()
                     if doc.exists:
                         discord_ids = doc.to_dict()
@@ -61,27 +59,30 @@ class TeamManager(commands.Cog):
                             sameTeam = True
                             break
 
-                    print(database_team_set)
-                    if not sameTeam and team_name in database_team_set:
+                    try:
+                        if not sameTeam and team_name in database_team_set:
+                            await inter.response.send_message(f"{team_name}'s Voice channel Created", ephemeral=True)
+                            #Create role for team
+                            await guild.create_role(name=f"Team {team_name}", colour=Color.from_rgb(0, 31, 63))
+                            print(f"{author.name} Created role 'Team {team_name}'")
+                            team_role = get(guild.roles, name=f"Team {team_name}")
 
-                        #Create role for team
-                        await guild.create_role(name=f"Team {team_name}", colour=Color.from_rgb(0, 31, 63))
-                        print(f"{author.name} Created role 'Team {team_name}'")
-                        team_role = get(guild.roles, name=f"Team {team_name}")
+                            #Assign team leader and team role to command excuter
+                            await author.add_roles(team_role)
+                            await author.add_roles(team_leader)
 
-                        #Assign team leader and team role to command excuter
-                        await author.add_roles(team_role)
-                        await author.add_roles(team_leader)
+                            overwrites[team_role] = PermissionOverwrite(connect=True)
 
-                        overwrites[team_role] = PermissionOverwrite(connect=True)
+                            #Create voice channel for team
+                            team_voice_channel = await guild.create_voice_channel(name=f"{team_name}'s Voice channel", overwrites=overwrites)
+                            print(f"{author.name} created {team_voice_channel.name} channel for team {team_name} .....")
+                            
+                        else:
+                            await inter.response.send_message(f"Sorry, could not find your team in the database. If you have registered, contact MLSC for help. Error Message", ephemeral=True)
 
-                        #Create voice channel for team
-                        team_voice_channel = await guild.create_voice_channel(name=f"{team_name}'s Voice channel", overwrites=overwrites)
-                        print(f"{author.name} created {team_voice_channel.name} channel for team {team_name} .....")
-                        await inter.response.send_message(f"Team's Voice channel Created", ephemeral=True),
-
-                    else:
-                        await inter.response.send_message(f"This team name already exist !!", ephemeral=True)
+                    except Exception as e:
+                        print(e)
+                        await inter.response.send_message("An error occurred while processing your request.", ephemeral=True)
 
                 except Exception as e:
                     print(e)
